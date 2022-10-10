99fe8b0610b354ecb1e7613f10feb1f77f299ccf
haedong.lim <haedong.lim@olive-hc.com>
2022-10-10 15:53:03 +0900

    chapter05 finish

--- kotlin-in-depth-ch05/src/main/kotlin/Main.kt
+++ kotlin-in-depth-ch05/src/main/kotlin/Main.kt
@@ -1,7 +1,50 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val consum = StringConsumer { s -> println(s) }
+    consum.accept("haedong")
+
+    println(lessThan(4, 5))
+
+    val loweCaseRoom = 'h'.isLowerCase()
+    println(loweCaseRoom)
+
+    println(check("Hello") { c -> c.isLetter() })
+    println(check("Hello") { it.isLowerCase() })
+
+    println("#######")
+
+    println(indexOf(intArrayOf(4, 3, 2, 1)) {
+        println("it is $it")
+        it < 3
+    })
+
+    println("####")
+    println("Hello".truncate(3))
 }
+
+fun String.truncate(maxLength: Int): String {
+    return if (length <= maxLength) this@truncate else substring(0, maxLength)
+}
+
+inline fun indexOf(numbers: IntArray, condition: (Int) -> Boolean): Int {
+    for (i in numbers.indices) {
+        if (condition(numbers[i])) return i
+    }
+
+    return -1
+}
+
+fun interface StringConsumer {
+    fun accept(s: String)
+}
+
+fun check(s: String, condition: (Char) -> Boolean): Boolean {
+    for (c in s) {
+        println("char c is : ${c}")
+        if (!condition(c)) return false
+    }
+
+    return true
+}
+
+val lessThan = { a: Int, b: Int -> a < b }
